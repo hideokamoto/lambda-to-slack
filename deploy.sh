24e9103832a168bfcfbcd07f90f18823d67f927b
hideokamoto <hide.okamoto@digitalcube.jp>
2018-05-08 22:00:23 +0900

    set region

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 aws cloudformation package --template-file ./template.yml --output-template-file template-output.yml --s3-bucket $S3_BUCKET
 aws cloudformation deploy \
- --template-file ./template-output.yml --stack-name lambda-slack --capabilities CAPABILITY_IAM --region us-east-1 \
+ --template-file ./template-output.yml --stack-name lambda-slack --capabilities CAPABILITY_IAM --region $AWS_REGION \
  --parameter-overrides SlackToken=$SLACK_TOKEN SlackChanel=$SLACK_CHANEL SlackPath=$SLACK_PATH SlackUsername=$SLACK_USERNAME
