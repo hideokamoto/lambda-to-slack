44dbc9e8a4c008c46f554106ee790f99cde82f50
hideokamoto <hide.okamoto@digitalcube.jp>
2018-05-08 21:42:31 +0900

    add incomming webhook lambda

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 aws cloudformation package --template-file ./template.yml --output-template-file template-output.yml --s3-bucket $S3_BUCKET
 aws cloudformation deploy \
  --template-file ./template-output.yml --stack-name lambda-slack --capabilities CAPABILITY_IAM --region us-east-1 \
- --parameter-overrides SlackToken=$SLACK_TOKEN SlackChanel=$SLACK_CHANEL
+ --parameter-overrides SlackToken=$SLACK_TOKEN SlackChanel=$SLACK_CHANEL SLACK_PATH=$SLACK_PATH SLACK_USERNAME=$SLACK_USERNAME
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const { WebClient } = require('@slack/client')
-
 const handler = (event, context, callback) => {
+  const { WebClient } = require('@slack/client')
   const { SLACK_TOKEN, SLACK_CHANEL } = process.env
   if (!SLACK_TOKEN || !SLACK_CHANEL) return callback(new Error('slack token or channel is undefined'))
   const { message, attachments } = event
@@ -26,3 +25,38 @@ const handler = (event, context, callback) => {
 }
 
 module.exports.handler = handler
+
+const incommingHandler = (event, context, callback) => {
+  const { SLACK_PATH, SLACK_CHANEL, SLACK_USERNAME } = process.env
+  if (!SLACK_PATH || !SLACK_CHANEL) return callback(new Error('slack path or channel not defined'))
+  const { message, attachments, emoji } = event
+  if (!message) return callback(new Error('message not defined.'))
+  const username = SLACK_USERNAME || 'slack_bot'
+  const postData = {
+    channel: SLACK_CHANEL,
+    username,
+    text: message,
+    icon_emoji: emoji || ':ghost:'
+  }
+  if (attachments) postData.attachments = attachments
+  const options = {
+    method: 'POST',
+    hostname: 'hooks.slack.com',
+    port: 443,
+    path: SLACK_PATH
+  }
+  const https = require('https')
+  const utils = require('util')
+  const req = https.request(options, res => {
+    res.setEncoding('utf8')
+    res.on('data', () => callback(null, true))
+  })
+
+  req.on('error', e => {
+    console.log('problem with request: ' + e.message)
+  })
+
+  req.write(utils.format('%j', postData))
+  req.end()
+}
+module.exports.incommingHandler = incommingHandler
