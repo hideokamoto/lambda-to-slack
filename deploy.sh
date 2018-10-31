e1aa8cdd54aa78c5224a4224bf54c2964b336928
hideokamoto <hide.okamoto@digitalcube.jp>
2018-10-31 16:31:50 +0900

    chanel selectable

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ aws cloudformation package --template-file ./template.yml --output-template-file
 aws cloudformation deploy \
  --template-file ./template-output.yml --stack-name lambda-slack-$STAGE --capabilities CAPABILITY_IAM --region $AWS_REGION \
  --parameter-overrides SlackToken=$SLACK_TOKEN SlackChanel=$SLACK_CHANEL SlackChanelDev=$SLACK_CHANEL_DEV SlackPath=$SLACK_PATH SlackUsername=$SLACK_USERNAME Stage=$STAGE
+aws cloudformation describe-stack-resources  --stack-name lambda-slack-$STAGE
--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const getChanel = (prod, dev = '', stage = 'development') => {
 const handler = (event, context, callback) => {
   const { WebClient } = require('@slack/client')
   const { SLACK_TOKEN, SLACK_CHANEL, STAGE, SLACK_CHANEL_DEV } = process.env
-  if (!SLACK_TOKEN || !SLACK_CHANEL) return callback(new Error('slack token or channel is undefined'))
+  if (STAGE !== 'production') console.log('Event: %j', event)
+  const slackChanel = event.slackChanel || SLACK_CHANEL
+  const slackDevChanel = event.slackDevChanel || SLACK_CHANEL_DEV
+  if (!SLACK_TOKEN || !slackChanel) return callback(new Error('slack token or channel is undefined'))
   const { message, attachments } = event
   if (!message) return callback(new Error('Message is undefined.'))
 
   const web = new WebClient(SLACK_TOKEN)
   const param = {
-    channel: getChanel(SLACK_CHANEL, SLACK_CHANEL_DEV, STAGE),
+    channel: getChanel(slackChanel, slackDevChanel, STAGE),
     text: message
   }
   if (attachments) param.attachments = attachments
@@ -33,12 +36,15 @@ module.exports.handler = handler
 
 const incommingHandler = (event, context, callback) => {
   const { SLACK_PATH, SLACK_CHANEL, SLACK_USERNAME, STAGE, SLACK_CHANEL_DEV } = process.env
-  if (!SLACK_PATH || !SLACK_CHANEL) return callback(new Error('slack path or channel not defined'))
+  if (STAGE !== 'production') console.log('Event: %j', event)
+  const slackChanel = event.slackChanel || SLACK_CHANEL
+  const slackDevChanel = event.slackDevChanel || SLACK_CHANEL_DEV
+  if (!SLACK_PATH || !slackChanel) return callback(new Error('slack path or channel not defined'))
   const { message, attachments, emoji } = event
   if (!message) return callback(new Error('message not defined.'))
   const username = SLACK_USERNAME || 'slack_bot'
   const postData = {
-    channel: getChanel(SLACK_CHANEL, SLACK_CHANEL_DEV, STAGE),
+    channel: getChanel(slackChanel, slackDevChanel, STAGE),
     username,
     text: message,
     icon_emoji: emoji || ':ghost:'
